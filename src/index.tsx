76ad4c23900cd8234d3cc08b49e6ee74a99b8944
Toki Urata <tokiurata@outlook.com>
2022-10-09 17:38:21 -0400

    Implement in-browser bundling

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,48 @@
-import React from 'react';
+import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom/client';
+import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from './plugin/unpkg-path-plugin';
 
 const App = () => {
-  return <h1>JBOOK APP</h1>
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    });
+  };
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()]
+    });
+
+    console.log(result);
+    setCode(result.outputFiles[0].text);
+  };
+
+  return (
+    <div>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(
