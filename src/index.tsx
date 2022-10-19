95622efb8cf6d4f1be3f56a545facf5a34bec5c1
Toki Urata <tokiurata@outlook.com>
2022-10-19 09:32:27 -0400

    Implement iframe

--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,8 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
-
-  useEffect(() => {
-    startService();
-  }, []);
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -20,10 +16,17 @@ const App = () => {
     });
   };
 
+  useEffect(() => {
+    startService();
+  }, []);
+
   const onClick = async () => {
     if (!ref.current) {
       return;
     }
+
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -35,20 +38,43 @@ const App = () => {
       }
     });
 
-    console.log(result);
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.getElementById('root');
+              root.innerHTML = '<div style="color: red"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
     <div>
-      <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+      <textarea 
+        value={input} 
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe ref={iframe} sandbox='allow-scripts' srcDoc={html} title='preview' />
     </div>
   );
-}
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
