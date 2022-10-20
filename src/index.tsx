2f04b122b62a7b50d6580ebc331b03339d1f7070
Toki Urata <tokiurata@outlook.com>
2022-10-20 08:49:58 -0400

    Create code-cell

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,14 @@
-import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
-import CodeEditor from './components/code-editor';
-import Preview from './components/preview';
-import bundler from './bundler';
+import CodeCell from './components/code-cell';
 
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
 const App = () => {
-  const [code, setCode] = useState('');
-  const [input, setInput] = useState('');
-
-  const onClick = async () => {
-    const output = await bundler(input);
-    setCode(output);
-  };
-
   return (
     <div>
-      <CodeEditor 
-        initialValue='const num = 1;' 
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
+      <CodeCell />
     </div>
   );
 };
