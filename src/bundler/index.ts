f69b7fa0eb19e5a9ab4200224c47cc3cddec169d
Toki Urata <tokiurata@outlook.com>
2022-10-20 08:35:39 -0400

    Organize files

--- src/bundler/index.ts
+++ src/bundler/index.ts
@@ -1,6 +1,6 @@
 import * as esbuild from 'esbuild-wasm';
-import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin';
-import { fetchPlugin } from '../plugins/fetch-plugin';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
 let service: esbuild.Service;
 
@@ -10,7 +10,7 @@ const bundler = async (rawCode: string) => {
       worker: true,
       wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
     });
-  }
+  };
 
   const result = await service.build({
     entryPoints: ['index.js'],
