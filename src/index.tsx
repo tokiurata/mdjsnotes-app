dfdde6330e976de48acb8bfdef5b4a67b1942d3e
Toki Urata <tokiurata@outlook.com>
2022-10-21 09:57:34 -0400

    Implement md text editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import ReactDOM from 'react-dom/client';
 
-import CodeCell from './components/code-cell';
+// import CodeCell from './components/code-cell';
+import TextEditor from './components/text-editor';
 
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
 const App = () => {
   return (
     <div>
-      <CodeCell />
+      <TextEditor />
+      {/* <CodeCell /> */}
     </div>
   );
 };
