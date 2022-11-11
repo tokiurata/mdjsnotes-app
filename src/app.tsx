eb113b9cc87ec0a3af03c5874f514bf255f55c26
Toki Urata <tokiurata@outlook.com>
2022-11-11 10:23:36 -0500

    Add Logo

--- src/app.tsx
+++ src/app.tsx
@@ -2,17 +2,22 @@ import { Provider } from 'react-redux';
 import { store } from './state';
 
 import CellList from './components/Cell-list/cell-list';
+import Logo from './components/Logo/logo';
 
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import './app.css';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <div>
+      <div className="app">
         <main>
           <CellList />
         </main>
+        <footer>
+          <Logo />
+        </footer>
       </div>
     </Provider>
   );
---
+++ src/components/Logo/logo.tsx
@@ -0,0 +1,7 @@
+import './logo.css';
+
+const Logo = () => {
+  return <h1 className="logo">MDJS Notes</h1>;
+};
+
+export default Logo;
